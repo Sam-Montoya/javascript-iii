169c9ba4cdda11caf2cfbadcac751f74e6a19b3b
Samuel <samuel.montoya1073@gmail.com>
2017-08-09 23:32:34 -0600

    Finished the assignment.

--- forInLoops_Delete.js
+++ forInLoops_Delete.js
@@ -7,23 +7,23 @@
 
 // In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+for(var key in values) {
+  //console.log(values[key])
+}
 
 // In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for(var key in values) {
+  //console.log(key)
+}
 
 
 // ========================
@@ -32,40 +32,68 @@
 // Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 
 function showValues( obj ) {
-  // CODE HERE
+  var myString = "";
+  for(var key in obj){
+      myString += obj[key];
+  }
+    return myString;
+}
+// ========================
+
+
+// Write a function called greaterThan10 that takes in an object. 
+//Write a for in loop that loops over the object and changes any value that is great than 10 to 0. Return the updated object.
+
+// CODE HERE
+
+function greaterThan10(obj){
+  for(var key in obj){
+    if(obj[key] > 10)
+      obj[key] = 0;
+  }
+  return obj;
 }
 
-
 // ========================
 
 
-// Write a function called greaterThan10 that takes in an object. Write a for in loop that loops over the object and changes any value that is great than 10 to 0. Return the updated object.
+// Write a function called double that takes in an object. 
+//Write a for in loop that loops over the object and changes every value to be itself multipled by 2. Return the updated object.
 
 // CODE HERE
 
-
+function double(obj){
+  for(var key in obj){
+    obj[key] = obj[key] * 2;
+  }
+  return obj;
+}
 
 // ========================
 
 
-// Write a function called double that takes in an object. Write a for in loop that loops over the object and changes every value to be itself multipled by 2. Return the updated object.
+// Write a function called secrets that will take in an object. Create an empty string variable. 
+//Write a for in loop that loops over the object. 
+//If the property name starts with an 'sh', concatenate the value to the string variable. 
+//By the end of the for in loop, you should have a sentence, return that sentence.
 
 // CODE HERE
 
-
+function secrets(obj ){
+  var myString = "";
+  for(var key in obj){
+    if(key.includes("sh")){
+      myString += obj[key];
+    }
+  }
+  return myString;
+}
 
 // ========================
 
 
-// Write a function called secrets that will take in an object. Create an empty string variable. Write a for in loop that loops over the object. If the property name starts with an 'sh', concatenate the value to the string variable. By the end of the for in loop, you should have a sentence, return that sentence.
-
-// CODE HERE
-
-
-// ========================
-
-
-// Sometimes it's needed to delete object properties. All you need is the word delete before a reference to the object property value. Uncomment the example below to see a for in loop deleting all the properties inside an object.
+// Sometimes it's needed to delete object properties. All you need is the word delete before a reference to the object property value. 
+//Uncomment the example below to see a for in loop deleting all the properties inside an object.
 
 // var deleteAllThethings = {
 //   one: 1,
@@ -86,7 +114,10 @@ function showValues( obj ) {
 
 // CODE HERE
 
-
+function removePassword(obj){
+  delete obj.password;
+  return obj;
+}
 
 // ========================
 
@@ -102,20 +133,42 @@ var deleteTheBigNumbers = {
 
 // CODE HERE
 
+for(var key in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[key] > 100)
+    delete deleteTheBigNumbers[key];
+}
 
 // ========================
 
 
-// Write a function called startsWithK that takes an object as a parameter. Write a for in loop to loop over the object. If any property name starts with k, delete that property. Return the updated object.
+// Write a function called startsWithK that takes an object as a parameter. Write a for in loop to loop over the object. 
+//If any property name starts with k, delete that property. Return the updated object.
 
 // CODE HERE
 
-
+function startsWithK(obj){
+  for(var key in obj){
+    if(key[0] === "k"){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 // ========================
 
 
-// Write a function called hiddenTreasure that takes in an object. Write a for in loop that loops over this object. Each property will have a sentence as it's value. If the property value does not contain the word 'treasure', delete the property. Return the updated object.
+// Write a function called hiddenTreasure that takes in an object. Write a for in loop that loops over this object. 
+//Each property will have a sentence as it's value. If the property value does not contain the word 'treasure', delete the property. 
+//Return the updated object.
 // (hint: the method includes() may be of use...)
 
 // CODE HERE
+function hiddenTreasure(obj){
+  for(var key in obj){
+    if(!obj[key].includes("treasure")){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
